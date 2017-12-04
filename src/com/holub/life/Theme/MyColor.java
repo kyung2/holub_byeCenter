d39c5141cee0685ced7ff12bfe150b3ed80d29b5
kyung2 <lovelyhyunkyung@gmail.com>
2017-12-04 18:39:31 +0900

    현경 변경 완료 이제 현경 push 끝

--- src/com/holub/life/Theme/MyColor.java
+++ src/com/holub/life/Theme/MyColor.java
@@ -12,13 +12,6 @@ public class MyColor {
 
     //default조정
 
-    private static Color LIVE_COLOR = Color.RED;
-    private static Color BORDER_COLOR = Colors.DARK_RED;
-    private static Color DEAD_COLOR = Colors.LIGHT_YELLOW;
-    private static Color BackGround_COLOR = Colors.ALICEBLUE;
-    private static Color GRAPHIC_COLOR = Colors.LIGHT_ORANGE;
-    private static Color GRAPHIC_SELECTION_COLOR = Color.BLUE;
-
     private static MyColor ourInstance = new MyColor();
 
     public static MyColor getInstance() {
